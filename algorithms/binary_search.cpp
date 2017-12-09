8554f5b294d1d2547d3e51fe125bb3d8c28b78cb
maojr <u013905837@gmail.com>
2017-12-10 00:51:01 +0800

    Add the recursive version of binary_search

--- algorithms/binary_search.cpp
+++ algorithms/binary_search.cpp
@@ -18,10 +18,27 @@ int binary_search(int* a, int length, int val)
     return -1;
 }
 
+// the recursive version
+// interview at http://aqrose.com/
+int recursive_binary_search(int* arr, int start, int end, int val)
+{
+    if (start > end) return -1;
+
+    int mid = start + (end - start) / 2;
+    if (arr[mid] == val)
+        return mid;
+    if (arr[mid] > val)
+        return recursive_binary_search(arr, start, mid - 1, val);
+    if (arr[mid] < val)
+        return recursive_binary_search(arr, mid + 1, end, val);
+}
+
 int main()
 {
     int a[7] = {1, 3, 4, 5, 6, 7, 9};
-    int result = binary_search(a, sizeof(a)/sizeof(a[0]), 110);
+
+    // int result = binary_search(a, sizeof(a)/sizeof(a[0]), 110);
+    int result = recursive_binary_search(a, 0, 6, 9);
 
     if(result == -1)
         std::cout << "Not find!" << std::endl;
