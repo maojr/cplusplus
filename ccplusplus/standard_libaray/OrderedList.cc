0d11a6f5a644ccacf954c24a240444486b418ffa
maojr <csucoder@163.com>
2017-12-10 15:11:53 +0800

    extend std::list

---
+++ ccplusplus/standard_libaray/OrderedList.cc
@@ -0,0 +1,32 @@
+// extend std::list
+
+#include <list>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+class OrderedList : public std::list<T>
+{
+public:
+    void add(const T& v) {
+        auto it = this->begin();
+        while (it != this->end() && *it < v)
+            ++it;
+        this->insert(it, v);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    OrderedList<int> list;
+
+    list.add(3);
+    list.add(1);
+    list.add(5);
+
+    for (auto & val : list)
+        std::cout << val << std::endl;
+    return 0;
+}
