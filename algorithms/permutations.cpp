29d53e7d2f4eb49cefcc1ef3f7e842189a0c71d1
maojr <csucoder@163.com>
2017-12-22 21:35:20 +0800

    bugfix & relearn the usage of next_permutation

--- algorithms/permutations.cpp
+++ algorithms/permutations.cpp
@@ -1,7 +1,6 @@
 // Q: 生成 N 个不同元素全排列
 // 这是 next_permutation() 的基本用法，把元素从小到大放好（即字典序最小的排
 // 列），然后反复调用 next_permutation()。
-
 #include <vector>
 #include <iostream>
 #include <iterator>
@@ -9,14 +8,15 @@
 
 int main()
 {
-    int elements[] = {1, 2, 3, 4};
-    const size_t N = sizeof(elements)/sizeof(elements[0]);
+    int elements[] = {1, 3, 2, 4};
+    const size_t N = sizeof(elements) / sizeof(elements[0]);
     std::vector<int> vec(elements, elements + N);
+
+    std::sort(vec.begin(), vec.end()); // why sort before next_permutation?
 
     do
     {
-        std::copy(vec.begin(), vec.end(),
-        std::ostream_iterator<int>(std::cout, ", "));
+        std::copy(vec.begin(), vec.end(), std::ostream_iterator<int>(std::cout, ", "));
         std::cout << std::endl;
     } while (next_permutation(vec.begin(), vec.end()));
 
