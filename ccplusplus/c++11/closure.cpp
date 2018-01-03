e1828ea9cc8e5668a99dd954a8da70ba94ef4b63
maojr <csucoder@163.com>
2018-01-04 00:39:23 +0800

    implement C++ closure with operator ()

--- ccplusplus/c++11/closure.cpp
+++ ccplusplus/c++11/closure.cpp
@@ -12,10 +12,24 @@ std::function<int()> closure_addby(int val, int step) {
     return f;
 }
 
+class closure
+{
+public:
+    closure(int val = 0) : start(val) {};
+    int operator() (int x) { return start += x; }
+private:
+    int start;
+};
+
 int main(int argc, char const *argv[])
 {
     auto add2 = closure_addby(11, 2);
     std::cout << add2() << std::endl;
     std::cout << add2() << std::endl;
+
+    closure cl(0);
+    std::cout << cl(1) << std::endl;
+    std::cout << cl(2) << std::endl;
+    std::cout << cl(3) << std::endl;
     return 0;
 }
