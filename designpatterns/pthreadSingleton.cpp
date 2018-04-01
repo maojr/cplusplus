f2d70e625437871f17c03b1ab7944013d7042995
maojr <csucoder@163.com>
2018-04-01 13:06:25 +0800

    implement Singleton with pthread_once

---
+++ designpatterns/pthreadSingleton.cpp
@@ -0,0 +1,46 @@
+#include <pthread.h>
+#include <iostream>
+
+class Foo {
+public:
+    Foo() {
+        std::cout << "Foo()" << std::endl;
+    }
+    ~Foo() {
+        std::cout << "~Foo()" << std::endl;
+    }
+};
+
+template <typename T>
+class Singleton {
+public:
+    static T& instance() {
+        pthread_once(&ponce_, &Singleton::init);
+        return *value_;
+    }
+private:
+    Singleton();
+    ~Singleton();
+
+    static void init()
+    {
+        value_ = new T();
+    }
+
+private:
+    static pthread_once_t ponce_;
+    static T* value_;
+};
+
+template <typename T>
+pthread_once_t Singleton<T>::ponce_ = PTHREAD_ONCE_INIT;
+
+template <typename T>
+T* Singleton<T>::value_ = NULL;
+
+int main(int argc, char const *argv[])
+{
+    Foo& foo = Singleton<Foo>::instance();
+    Foo& bar = Singleton<Foo>::instance();
+    return 0;
+}
