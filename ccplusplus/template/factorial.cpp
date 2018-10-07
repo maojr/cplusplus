7a37d1ee0a7f32cb5e23565338b73203f0b94ede
maojr <csucoder@163.com>
2018-10-07 23:51:36 +0800

    Template: factorial

---
+++ ccplusplus/template/factorial.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <unsigned int n> struct Factorial {
+  enum { value = n * Factorial<n - 1>::value };
+};
+
+template <> struct Factorial<0> {
+  enum { value = 1 };
+};
+
+int main(int argc, char const *argv[]) {
+  std::cout << Factorial<10>::value << std::endl;
+  return 0;
+}
